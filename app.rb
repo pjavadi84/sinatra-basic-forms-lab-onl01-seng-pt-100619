43360629e93a7af363bfa784b366be0d95eb80db
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-24 04:06:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require_relative 'config/environment'
+require_relative 'models/puppy.rb'
 
 class App < Sinatra::Base
+ 
+    get '/' do
+      erb :index
+    end
+    
+    get '/new' do
+      erb :create_puppy
+    end
+    
+    post '/poppy' do
+      
+    end
 
 end
